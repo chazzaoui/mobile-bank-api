f056780b449cf42d4a07714c5726840458075490
chaffie <c.azzaoui@live.nl>
2024-07-19 19:40:06 +0200

    Set STATIC_ROOT and STATIC_URL in settings.py

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
